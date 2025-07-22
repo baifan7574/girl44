ef8484f1888674db7efcd7357efcd3d52fbfbf7a
baifan7574 <baifan7574@gmail.com>
2025-07-23 07:52:26 +0800

    One, two.

--- run_site_builder_v12.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title 图站构建 · 脚本12 · 单图+广告+导航+SEO
-python auto_generate_v12_single_ads.py
-pause
---
+++ run_v12_universal.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+python auto_generate_v12_single_ads_fix.py
+pause
